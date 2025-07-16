9f3b2e1ee4b1368485b5ff2e1c66bc286c779c7e
Valentin <valentin.gaillard@ecole-isitech.fr>
2025-07-16 09:36:34 +0200

    feat: initializing database
    Using mysql with prisma

---
+++ apps/backend/prisma/migrations/20250716073301_init/migration.sql
@@ -0,0 +1,23 @@
+-- CreateTable
+CREATE TABLE `Game` (
+    `id` VARCHAR(191) NOT NULL,
+    `target` VARCHAR(191) NOT NULL,
+    `maxGuesses` INTEGER NOT NULL DEFAULT 6,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Guess` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `gameId` VARCHAR(191) NOT NULL,
+    `guess` VARCHAR(191) NOT NULL,
+    `result` JSON NOT NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Guess` ADD CONSTRAINT `Guess_gameId_fkey` FOREIGN KEY (`gameId`) REFERENCES `Game`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
